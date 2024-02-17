96d926222dd9fe5b42d728444fafcc9fe66da2a7
GustavoSFer <gustavo.sfernander@gmail.com>
2024-02-17 13:27:26 -0300

    Feat: Criando o menu do header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faGear, faLock, faPersonArrowUpFromLine, faPersonWalkingArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+
 
 function Header() {
+    const [menu, setMenu] = useState(false);
+
+    const menuHeader = () => {
+        console.log("clicado")
+        setMenu(!menu);
+        console.log(menu)
+    }
+
     return (
         <header className='border border-primary p-2'>
             <div className='text-end'>
-                logout
+                <FontAwesomeIcon icon={faBars} onClick={menuHeader} />
+                {
+                    menu &&
+                    <div className='position'>
+                        <p>
+                            <Link to={"/sobreSistema"} className='link'>
+                            <FontAwesomeIcon icon={faLock} onClick={menuHeader} className='me-2' />
+                                Sobre o sistema
+                            </Link>
+                        </p>
+                        <p>
+                            <Link to={"/"} className='link'>
+                                <FontAwesomeIcon icon={faPersonWalkingArrowRight} onClick={menuHeader} className='me-2' />
+                                Logout
+                            </Link>
+                        </p>
+                    </div>                    
+                }
             </div>
         </header>
     );
